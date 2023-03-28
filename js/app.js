9b0de3abee460b0921f5f640b3d57fe52db8eaea
Miguel Ruiz <later481@gmail.com>
2023-03-27 22:17:51 -0600

    variables formulario

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const d = document;
+
+d.addEventListener("DOMContentLoaded", ()=>{
+    const formulario = d.querySelector("#formulario");
+    const btnSubmit = d.querySelector("#formulario button[type='submit']");
+    const inputName = d.querySelector("#name");
+    const inputEmail = d.querySelector("#email");
+    const inputSubject = d.querySelector("#subject");
+    const message = d.querySelector("#message");
+
+    // console.log(inputEmail);
+});
